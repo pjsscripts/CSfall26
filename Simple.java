fc6a8c76549c505ebe5ccbb961243eb4549515a0
pjsscripts <Pascalpattonimani@icloud.com>
2026-08-25 11:43:49 -0500

    blah blah

---
+++ Simple.java
@@ -0,0 +1,18 @@
+public class Simple
+{ 
+        public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+//white space doesent matter java is based on curly braces for denoting sections
+//puclic static class is notation for functions
+/*
+this is how you
+do multi 
+line
+comments
+ */
+//all applications require a main function
+//all lines of code other than classes need a semi colon 
+//must declare variables 
